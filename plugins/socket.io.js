d01516b4ee58bd78d4a9d219831fd31ce0a59f65
Dg <dogeunchoi@gmail.com>
2018-05-04 16:20:49 +0900

    Modify package json

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client'
-const socket = io(('http://' + process.env.HOST + process.env.PORT) || 'http://127.0.0.1:3000')
+const socket = io(('http://' + process.env.HOST + ':' + process.env.PORT) || 'http://127.0.0.1:3000')
 
 export default socket
